d08bbdce0bed5adc3f6c87d57831cb685b61d7bb
ranjeetkumarpro <ranjeetkumar9464@gmail.com>
2025-11-20 12:10:26 +0530

    Add files via upload

---
+++ Comparison_Operator.sql
@@ -0,0 +1,9 @@
+## Comparison Operators
+Select * from personal where age>18;
+Select * from personal where age=18;
+Select * from personal where age<20;
+Select * from personal where age>=18;
+Select * from personal where age<=18;
+Select * from personal where city="Agra";
+select * from personal where city!="agara";
+Select * from personal where city<>"Agra";
