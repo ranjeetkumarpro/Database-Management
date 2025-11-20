fc707cb61b5c35ba48fff3430461646fda6da460
ranjeetkumarpro <ranjeetkumar9464@gmail.com>
2025-11-20 19:54:15 +0530

    Add files via upload

---
+++ Arithmetic_Functions.sql
@@ -0,0 +1,58 @@
+## Arithmetic functions
+
+select 5+6;
+
+select 5-6;
+
+select 5*6;
+
+select 5/3;
+
+select * from personal1;
+
+select id, name, (percantage +5) as new_percantage from personal1;
+
+select pi();
+
+select round(4.49);
+
+select round(-4.49);
+
+select round(1234.987,2);
+
+select round(1234.987,1);
+
+select ceil(1.23);
+
+select floor(4.56);
+
+select pow(2,2);
+
+select pow(2,3);
+
+select pow(2,4);
+
+select sqrt(16);
+
+select sqrt(4);
+
+select sqrt(5);
+
+select round(sqrt(9),2);
+
+select rand();
+
+
+select rand()*100;
+
+select abs(-56);
+
+select abs(-56.25);
+
+select abs(56.25);
+
+select sign(23);
+
+select sign(0);
+
+select sign(-23);
