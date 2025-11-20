2ad3e2b0388210e33a66f18ef6a157c7baba2717
ranjeetkumarpro <ranjeetkumar9464@gmail.com>
2025-11-20 20:07:02 +0530

    Add files via upload

---
+++ Date_And_Time_Function.sql
@@ -0,0 +1,74 @@
+ # Date Function
+ 
+select current_date();
+
+select curdate();
+
+select sysdate();
+
+select now();
+
+select date("2025-10-14 10:09:21");
+
+select month("2025-10-14 10:09:21");
+
+select monthname("2025-10-14 10:09:21");
+
+select year("2025-10-14 10:09:21");
+
+select quarter("2025-10-14 10:09:21");
+
+select day("2025-10-14 10:09:21");
+
+select dayofmonth("2025-10-14 10:09:21");
+
+select dayname("2025-10-14 10:09:21");
+
+select dayofweek("2025-10-14 10:09:21");
+
+select yearweek("2025-10-14 10:09:21");
+
+select last_day("2025-10-14 10:09:21");
+
+select extract(month from "2025-10-14 10:09:21");
+
+select extract(year from "2025-10-14 10:09:21");
+
+select extract(week from "2025-10-14 10:09:21");
+
+select extract(day from "2025-10-14 10:09:21");
+
+select extract(hour from "2025-10-14 10:09:21");
+
+select extract(minute from "2025-10-14 10:09:21");
+
+select extract(second from "2025-10-14 10:09:21");
+
+select adddate("2025-10-14", interval 10 day);
+
+select adddate("2025-10-14", interval 1 month);
+
+select adddate("2025-10-14", interval 1 year);
+
+select adddate("2025-10-14", interval 500 minute);
+
+select date_add("2025-10-14", interval 10 day);
+
+select subdate("2025-10-14", interval 1 day);
+
+select datediff("2025-10-14", "2025-10-13");
+
+## Time Function
+
+
+select current_time();
+
+select curtime();
+
+select current_timestamp();
+
+select time("2025-10-14 10:09:2021");
+
+select hour("2025-10-14 10:09:2021");
+
+select min("2025-10-14 10:09:2021");
